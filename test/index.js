b517da1bd4e14e5411e393a356a030386820d603
yueluo <yueluo.yang@qq.com>
2021-08-28 07:12:16 +0800

    feat: 编写代理功能

--- test/index.js
+++ test/index.js
@@ -1,7 +1,3 @@
-const tarsRpc = require('@tars/rpc');
-
-const Test = require('./HelloProxy');
-
 const registry = require('@tars/registry');
 
 registry.setLocator("tars.tarsregistry.QueryObj@tcp -h 39.99.162.213 -p 17890");
