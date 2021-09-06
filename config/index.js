71be7774e9cfb0d08cd445a0b7cd05840eb59072
杨志强 <yangzhiqiang@winwin-inc.com>
2021-09-06 09:45:43 +0800

    perf: update

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@
  */
 
 const SERVICE = "tars.tarsregistry.QueryObj@tcp";
-const DOMAIN = "39.99.162.213";
-const PORT = "17890";
+const DOMAIN = "127.0.0.1";
+const PORT = "17892";
 
 const REPORT_DOMAIN = "127.0.0.1";
 
